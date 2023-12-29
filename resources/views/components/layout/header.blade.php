7c50755864690b5de4f045297452974e34002fcb
BadreddineZatout <hb_zatout@esi.dz>
2023-12-29 19:12:08 +0100

    feat: add header

---
+++ resources/views/components/layout/header.blade.php
@@ -0,0 +1,8 @@
+<div class="w-full h-16 flex justify-start items-center px-5 py-3 shadow-md bg-japonica-400 text-lg overflow-hidden">
+    <div>
+        <h1 class="text-2xl font-bold">
+            <a href="/"><img class="h-12" src="/images/logo.png" alt="Medical Technologies And Solutions" /></a>
+        </h1>
+    </div>
+
+</div>
