aa092e4d7313764f41e0ab2c3c2a8c2f63230cfa
BadreddineZatout <hb_zatout@esi.dz>
2023-12-29 20:25:24 +0100

    add nav links

--- resources/views/components/sections/advantages.blade.php
+++ resources/views/components/sections/advantages.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-10 px-10">
+<div id="advantages" class="mt-10 px-10">
     <h1 class="text-4xl font-bold text-japonica-500 px-10">Nos Avantages</h1>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-5   mt-10">
         <div class="advantage">
--- resources/views/components/sections/services.blade.php
+++ resources/views/components/sections/services.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-10 px-10">
+<div id="services" class="mt-10 px-10">
     <h1 class="text-4xl font-bold text-japonica-500 px-10">Nos Services</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5   mt-10">
         <div class="service">
